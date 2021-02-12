9c71b70a4151cfbe7023a7d551f3bc40c193e975
pl98754321 <pream5138@gmail.com>
2021-02-13 05:28:28 +0700

    initial commit

---
+++ dj1_ork1/dj1_ork1_work1/views.py
@@ -0,0 +1,14 @@
+from os import name
+from django.contrib.admin import autodiscover
+from django.shortcuts import render
+from .models import aticlename
+
+# Create your views here.
+def hello(request):
+    return render(request,'index.html')
+
+def nameAticle(request):
+    #data from aticlename
+    name = request.POST["Aticle"]
+    inside = request.POST["insideAticle"]
+    return render(request,'nameAticle.html',{"nameindex2":name,"insideindex":inside})
